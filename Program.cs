34047509c31612f505eafbdc8a275be88767f018
macbook <macbook@Ruslan-mac.local>
2024-10-25 17:06:56 +0400

    added method tasks

---
+++ Program.cs
@@ -0,0 +1,58 @@
+﻿#region statictask
+// 1. Bir int parametr qəbul edən və o ədədə qədər bütün cüt ədədlərin cəmi
+
+// int netice = CutlerinCemi(8);
+// Console.WriteLine($"Cutlerin cemi: {netice}");
+
+// static int CutlerinCemi (int number)
+// {
+//     int sum = 0;
+//     for (int i = 0; i <= number; i++)
+//     {
+//         if (i % 2 == 0)
+//         {
+//             sum += i;
+//         }
+//     }
+//     return sum; 
+// }
+
+// static void CutlerinCeminiCap (int number)
+// {
+//     int sum = 0;
+//     for (int i = 0; i <= number; i++)
+//     {
+//         if (i % 2 == 0)
+//         {
+//             sum += i;
+//         }
+//     } Console.WriteLine($"Cutlerin cemi: {sum}");
+// }
+// CutlerinCeminiCap(10); 
+
+#endregion
+
+
+#region task2
+// 2. Sozlerin arasinda boslugu silmek.
+
+// static void PrintUnSpaceWord (string[] words)
+// {
+//     string word = "Yusif Guluzade";
+//     System.Console.WriteLine(UnSpace(word));
+// }
+// static string UnSpace (string letter)
+// {
+//     string netice = "";
+    
+//     for (int i = 0; i < letter.Length; i++) 
+//     {
+//         if (letter[i] != ' ')
+//         {
+//             netice += letter[i];
+//         } 
+//     }
+//     return netice;
+// }
+
+#endregion
